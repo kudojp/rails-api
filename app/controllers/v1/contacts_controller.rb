9524011b187bc68adfa9be8d988008f31ce68d85
kudojp <heyjudejudejude1968@gmail.com>
2020-03-04 01:25:48 +0900

    Implement create/destroy method of ContactsControllerr

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,6 +1,31 @@
 class V1::ContactsController < ApplicationController
     def index
         @contacts = Contact.all
+        
         render json: @contacts, status: :ok
+    end
+
+    def create
+        @contact = Contact.new(contact_params)
+        @contact.save
+
+        @contact.save
+        render json: @contact, status: :created
+    end
+
+    def destroy
+        @contact = Contact.where(id: params[:id]).first
+        if @contact.destroy
+            head(:ok)
+        else
+            head(:umprocessable_entity)
+        end
+    end
+
+
+    private
+
+    def contact_params
+        params.require(:Contact).permit(:first_name, :last_name, :email)
     end
 end
