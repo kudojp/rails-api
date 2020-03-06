7b118c44937c6561e7f95e2d818e4155804f5343
kudojp <heyjudejudejude1968@gmail.com>
2020-03-06 13:53:01 +0900

    Introduce jsbuilder and repalace sessions#create controller with it

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :v1 do
+  namespace :v1, defaults: { foramt: :json } do
     resource :sessions, only: [:create, :destroy]
     resources :contacts
   end
